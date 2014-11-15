b15d877393707dd348d7ade01e01ab03cd69336a
Dmitry <df.creative@gmail.com>
2014-11-15 15:05:44 +0300

    Add add test

--- test/index.js
+++ test/index.js
@@ -15,3 +15,7 @@ assert.deepEqual(m.between([-101, 101], [-50, -100], [100, 50]), [-50, 50]);
 //limit objects
 assert.deepEqual(m.between({top: 120, bottom: -10}, 0, 100), {top: 100, bottom: 0});
 assert.deepEqual(m.between({top: 120, bottom: -10}, {top: 0, bottom:0},  {top: 100, bottom: 100}), {top: 100, bottom: 0});
+
+//maths
+assert.deepEqual(m.sub({top:10,bottom:20}, {top:5, bottom:5}), {top:5, bottom:15});
+assert.deepEqual(m.add({top:10, bottom:20}, {top:5, bottom:5}), {top:15, bottom:25});
