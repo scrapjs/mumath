eb4bd7a5be0442ade45b9a3d14269e8dbc0c4cbb
Dmitry <df.creative@gmail.com>
2014-11-21 19:15:44 +0300

    Add some basic maths

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ module.exports = {
 	min: wrap(Math.min),
 	max: wrap(Math.max),
 	add: wrap(function(a,b){return a+b}),
-	sub: wrap(function(a,b){return a-b})
+	sub: wrap(function(a,b){return a-b}),
+	div: wrap(function(a,b){return a/b}),
+	mul: wrap(function(a,b){return a*b}),
+	mod: wrap(function(a,b){return a%b})
 };
 
 
