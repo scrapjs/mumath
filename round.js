b830dac4c98503cf43e41f35c9212ba8aed6307b
Dmitry <df.creative@gmail.com>
2014-11-28 23:07:18 +0300

    Fix runtime include

--- round.js
+++ round.js
@@ -11,11 +11,12 @@
  * toPrecision(213.34, .1) == 213.3
  * toPrecision(213.34, 10) == 210
  */
+var precision = require('./precision');
 
 module.exports = require('./wrap')(function(value, step) {
 	if (step === 0) return value;
 	if (!step) return Math.round(value);
 	step = parseFloat(step);
 	value = Math.round(value / step) * step;
-	return parseFloat(value.toFixed(require('./precision')(step)));
+	return parseFloat(value.toFixed(precision(step)));
 });
