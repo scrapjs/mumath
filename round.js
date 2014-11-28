8b39d6fcde68b86a87903bb1db3af08aeb96a846
Dmitry <df.creative@gmail.com>
2014-11-28 22:23:46 +0300

    Append forgotten round module

---
+++ round.js
@@ -0,0 +1,21 @@
+/**
+ * Precision round
+ *
+ * @param {number} value
+ * @param {number} step Minimal discrete to round
+ *
+ * @return {number}
+ *
+ * @example
+ * toPrecision(213.34, 1) == 213
+ * toPrecision(213.34, .1) == 213.3
+ * toPrecision(213.34, 10) == 210
+ */
+
+module.exports = function(value, step) {
+	if (step === 0) return value;
+	if (!step) return Math.round(value);
+	step = parseFloat(step);
+	value = Math.round(value / step) * step;
+	return parseFloat(value.toFixed(getPrecision(step)));
+};
